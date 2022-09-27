8164d67090d609ba3f598138ef7ca2aa274a2105
Esila-A <esila.adajewa@gmail.com>
2022-09-27 23:01:26 +0200

    scss changes, about text change

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,35 +5,42 @@ const About = () => {
           <h1 className="about_header">About me</h1>
           <p className="about_paragraph text">
             My name is Elsi Adajew. I am from Chechnya (from which my family
-            fled during the war in 1999), but I've been living in Poland for
-            22 years now. I have spent most of my life here, so I feel as much a
-            Pole as a Chechen.
+            fled during the war in 1999), but I've lived in Poland for 22 years
+            now. I have spent most of my life here, so I feel as much a Pole as
+            a Chechen.
             <br />
             <br />
-            Due to my origin and experience, for many years I've been
-            cooperating with organizations working for refugees, migrants and
-            human rights. I participate in events, conduct workshops, give
-            lectures and interviews sharing my experiences and thoughts. I am
-            also the president of the "Sintar Foundation", an NGO that runs the
+            Due to my origin and experience, over the years I've collaborated with
+            many organisations that work in the field of human rights and refugee aid.
+            I regularly conduct workshops, give lectures and interviews, and participate in events
+            sharing my experiences and knowledge in these fields. I am also
+            the president of the "Sintar Foundation", an NGO that runs the
             "Sintar Center" - an innovative cultural and educational center,
-            addressed mainly (but not exclusively) to Chechen children and
-            youth. The main activities of Sintar are extracurricular activities
-            that prepare the young generation of refugees to live in Poland and
-            respect the new homeland.
+            addressed mainly (but not exclusively) at Chechen children and
+            youth. The main focus of Sintar are extracurricular activities that
+            aim to prepare the young generation of refugees for a new life in
+            Poland, and help them integrate with the Polish society.
             <br />
             <br />
-            I have been interested in photography and film since I was a child.
-            In recent years I have taken it very seriously and have started to
-            develop myself in this field. I started my own business - Elsi Adajew
-            Connecting & Creating - and am employed as a photographer at both
-            private and public events. I take photos of people, animals,
-            products, landscapes. You can find examples of my works in the gallery
-            and on my Instagram.
+            I've been interested in photography and film since I was a child,
+            capturing the happy and peaceful moments and aspects of everyday life
+            throughout the almost constant turmoil surrounding me back then.
+            The hobby naturally evolved into my chosen profession, and I've been
+            developing myself in this field with an even greater dedication since the recent years.
+            I've started my own business - Elsi Adajew Connecting & Creating
+            - and work as a photographer at both private
+            and public events. I take photos of people, animals, products,
+            landscapes. You can find examples of my works in the gallery and on
+            my <a href="https://www.instagram.com/mr.pandaev/" target="_blank" rel="noreferrer" className="text_instaLink">
+              Instagram
+            </a>
+            .
             <br />
-            <br />I am very active on social media. I love being in touch
-            with people, meeting new friends and expanding my experiences and
-            knowledge. You can find me on Instagram, TikTok, FaceBook and Youtube.
-            Feel free to get in touch either in here or one of these platforms. 
+            <br />I am very active on social media. I love being in touch with
+            people, meeting new friends and expanding my experiences and
+            knowledge. You can find me on Instagram, TikTok, FaceBook and
+            Youtube. Feel free to get in touch either in here or one of these
+            platforms.
           </p>
         </div>
       </section>
