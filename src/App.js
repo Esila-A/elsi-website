e5c589f16ebc8cc7449968a03358bcebdea90fdc
Esila-A <esila.adajewa@gmail.com>
2022-09-27 08:07:49 +0200

    style changes, about text, sintar disable

--- src/App.js
+++ src/App.js
@@ -7,30 +7,30 @@ import About from "./pages/about";
 import Gallery from "./pages/gallery";
 import Collaborations from "./pages/collaborations";
 import Interviews from "./pages/interviews";
-import Sintar from "./pages/sintar";
+// import Sintar from "./pages/sintar";
 import Footer from "./components/footer";
 import Page from "./components/gallery/page";
 
 
 function App() {
-  return (
-      <>
-          <Header />
-          <main>
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="home" element={<Home />} />
-                  <Route path="about" element={<About />} />
-                  <Route path="gallery" element={<Gallery />} />
-                  <Route path="gallery/:galleryName" element={<Page />} />
-                  <Route path="collaborations" element={<Collaborations />} />
-                  <Route path="interviews" element={<Interviews />} />
-                  <Route path="sintar" element={<Sintar />} />
-              </Routes>
-          </main>
-          <Footer />
-      </>
-  );
+    return (
+        <>
+            <Header />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="home" element={<Home />} />
+                        <Route path="about" element={<About />} />
+                        <Route path="gallery" element={<Gallery />} />
+                        <Route path="gallery/:galleryName" element={<Page />} />
+                        <Route path="collaborations" element={<Collaborations />} />
+                        <Route path="interviews" element={<Interviews />} />
+                        {/* <Route path="sintar" element={<Sintar />} /> */}
+                    </Routes>
+                </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <div className="home_text">
                 <h1 className="home_header-one">Hi!</h1>
                 <h2 className="home_header-two">I'm Elsi</h2>
-                <p className="home_paragraph">I take photos, make videos and I love doing that! <br/> Hope you find what you're looking for!</p>
+                <p className="home_paragraph">I take photos, make videos and I love doing that! <br/> Need a photographer? get in touch!</p>
             </div>
         </div>
     )
